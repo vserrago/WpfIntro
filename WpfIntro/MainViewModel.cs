0551d4a249d609744630f6884a5d6aaa45e2a238
Valentin Serrago <valentinserrago@gmail.com>
2014-10-01 14:23:54 -0400

    Base classes

---
+++ WpfIntro/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+
+namespace WpfIntro
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ObservableCollection<Student> _students;
+
+        public MainViewModel()
+        {
+            _students = new ObservableCollection<Student>();
+        }
+
+        public ObservableCollection<Student> Students
+        {
+            get { return _students; }
+            set
+            {
+                _students = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
---
+++ WpfIntro/Student.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfIntro
+{
+    public class Student : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _studentNumber;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Student(string firstName, string lastName, string studentNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            StudentNumber = studentNumber;
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string StudentNumber
+        {
+            get { return _studentNumber; }
+            set
+            {
+                _studentNumber = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
